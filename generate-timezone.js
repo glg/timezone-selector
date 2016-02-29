a382d0198323bce54cb489ef5d1eb742ed859af2
Mark Stover <stovenator@gmail.com>
2016-02-29 15:26:19 -0600

    Parse cities and countries

--- generate-timezone.js
+++ generate-timezone.js
@@ -5,10 +5,68 @@ var path = require('path');
 var gutil = require('gulp-util');
 var PluginError = gutil.PluginError;
 var File = gutil.File;
+var utf8 = require('utf8');
 
 module.exports = function(options) {
     options = options || {};
     var fileContents = {};
+
+    var convertCities = function(){
+        var cities = {};
+        var cityArray = fileContents['cities15000.txt'].split(/\r?\n/);
+        for (var i=0; i < cityArray.length; i++){
+            var line = cityArray[i].trim().split("\t");
+            if (/\S/.test(line)){
+                var mainName = line[2];
+                var country = line[8];
+                var state = country == 'US' && line[10] || undefined;
+                var population = parseInt(line[14]);
+                var timezone = line[17];
+                var isCapital = line[7] == 'PPLC' || undefined;
+
+                var cityKey;
+                if (state){
+                    cityKey = country + '/' + mainName + '/' + state;
+                }
+                else{
+                    cityKey = country + '/' + mainName;
+                }
+                cityKey = cityKey.replace(' ', '_');
+                // There was already a city with that name, let the one
+                // with the higher population win.
+                if (!cities.hasOwnProperty(cityKey) || population > cities[cityKey].population){
+                    cities[cityKey] = {
+                        country: country,
+                        state: state,
+                        name: mainName,
+                        timezone: timezone,
+                        population: population,
+                        isCapital: isCapital
+                    };
+                }
+            }
+        }
+        return cities;
+    };
+
+    var convertCountries = function(){
+        var countries = {};
+        var countryArray = fileContents['countryInfo.txt'].split(/\r?\n/);
+        for (var i=0; i < countryArray.length; i++){
+            var line = countryArray[i].trim().split("\t");
+            if (line && line[0].charAt(0) !== '#'){
+                var countryCode = line[0];
+                var country = line[4];
+                var capital = line[5];
+                countries[countryCode] = {
+                    name: country,
+                    capital: capital,
+                    code: countryCode
+                };
+            }
+        }
+        return countries;
+    };
 
     function bufferContents(file, encoding, callback) {
 
@@ -31,9 +89,11 @@ module.exports = function(options) {
 
         var cities1000 = fileContents['cities1000.txt'];
         var cities15000 = fileContents['cities15000.txt'];
-        var countries = fileContents['countryInfo.txt'];
 
-        console.log(countries);
+        var countryData = convertCountries();
+        var cityData = convertCities();
+
+        console.log(cityData);
 
         var self = this;
         var timezoneFile = new File({
@@ -42,7 +102,7 @@ module.exports = function(options) {
         });
         self.push(timezoneFile);
         callback();
-    };
+    }
 
 
     return through.obj(bufferContents, endStream);
